28eb02051e21393e2dcc5d0db4137b293d653f0d
vinicius-caputo <74276895+vinicius-caputo@users.noreply.github.com>
2022-02-06 20:47:31 -0300

    Add files via upload

---
+++ B.java
@@ -0,0 +1,23 @@
+public class B {
+    private int B1;
+    private float B2;
+    
+    public void setB1(int B1){
+        this.B1 = B1;
+    }
+    public int getB1(){
+        return B1;
+    }
+    public void setB2(Float B2){
+        this.B2 = B2;
+    }
+    public float getB2(){
+        return B2;
+    }
+    public void MB1(){
+        System.out.println("MB1");
+    }
+    public void MB2(){
+        System.out.println("MB2");
+    }
+}
