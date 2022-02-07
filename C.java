7d0e7d9dd0daa4f6e424bf5af6d2dec5e14318ce
vinicius-caputo <vinicius.caputo@compjunior.com.br>
2022-02-06 21:23:46 -0300

    Criação da Classe C

---
+++ C.java
@@ -0,0 +1,11 @@
+public class C {
+    private String C1;
+    private int C2;
+
+    public void MC1(){
+        System.out.println("MC1");
+    }
+    public void MC2(){
+        System.out.println("MC2");
+    }
+}
