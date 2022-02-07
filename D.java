8e7ad6d74ee4872eeda6d820f32c7a2c83a8bf53
vinicius-caputo <vinicius.caputo@compjunior.com.br>
2022-02-06 22:29:48 -0300

    Criação da Classe C

---
+++ D.java
@@ -0,0 +1,12 @@
+public class D {
+    private String D1;
+    private int D2;
+
+    public void MD1(){
+        System.out.println("MD1");
+    }
+    public void MD2(){
+        System.out.println("MD2");
+    }
+
+}
